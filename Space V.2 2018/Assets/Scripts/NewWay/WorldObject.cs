f7c3d3bfe4cb401cdb65f82798057f8e47c50c39
Hyden17 <38673933+Hyden17@users.noreply.github.com>
2019-02-04 12:21:26 -0800

    More Fixing

---
+++ Space V.2 2018/Assets/Scripts/NewWay/WorldObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldObject : MonoBehaviour
+{
+//Basic, Add to this later.
+    public float health;
+    public int Level;
+    public Rigidbody RB;
+    public Object OnDestroyObject;
+    public Vector3 Position;
+    public bool UseGravity;
+    public string Type;
+    public bool OnUpdateFunction;
+    
+
+    
+//Fun Things (Controller References)
+    public float spawnrate;
+    public bool IsPlayerControlled;
+    public Object AIScript;
+    public GameObject Controller;
+    //CAS-g KS Future Functionality
+    public int EnergyMax;
+    public Dictionary<string, int> EnergyValues = new Dictionary<string, int>();
+    public int[] pattern;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
